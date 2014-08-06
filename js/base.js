d68eb4e7b2d22a5e61da054b9d73756a1fa92b8f
Rob Johnson <corndogcomputers@gmail.com>
2014-08-06 11:42:57 -0700

    more header fixes

--- js/base.js
+++ js/base.js
@@ -106,7 +106,7 @@
       if ($w <= 768) {
         $('.navbar').addClass('navbar-fixed-top');
         $('.navbar').css('opacity', '0.96');
-        $('body').css('margin-top', '59px');
+        $('body').css('margin-top', '56px');
       } else {
         $('.navbar').removeClass('navbar-fixed-top');
         $('.navbar').css('opacity', '1');
